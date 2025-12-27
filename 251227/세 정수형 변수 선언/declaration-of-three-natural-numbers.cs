6772d2cdccbd2d42249d0e7e9313c2247d5e5c40
Kyung-Hyun36 <66782867+Kyung-Hyun36@users.noreply.github.com>
2025-12-27 19:42:53 +0900

    Title: 세 정수형 변수 선언, Time: 57ms, Memory: 4MB, Status: Passed - Codetree

---
+++ 251227/세 정수형 변수 선언/declaration-of-three-natural-numbers.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        int a = 7;
+        int b = 23;
+        int c = 30;
+        
+        Console.WriteLine($"{a} + {b} = {c}");
+    }
+}
