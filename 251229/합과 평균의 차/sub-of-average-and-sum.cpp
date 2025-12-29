ac26d845b4ed977ce51fdbd8bfd9ec2d9514398b
Kyung-Hyun36 <66782867+Kyung-Hyun36@users.noreply.github.com>
2025-12-29 11:12:33 +0900

    Title: 합과 평균의 차, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251229/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int sum = a + b + c;
+    int avg = sum / 3;
+
+    cout << sum << "\n";
+    cout << avg << "\n";
+    cout << sum - avg;
+
+    return 0;
+}
