91234fcec3c0feb081678120817e12972dda0380
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-07 16:01:41 +0900

    Title: a로 채워넣기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260107/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+
+    word[1] = 'a';
+    word[word.length()-2] = 'a';
+
+    cout << word;
+
+    return 0;
+}
