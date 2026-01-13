4e81379aacd81fe3968d1ed8067bdc3307d233ea
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-13 10:33:04 +0900

    Title: 변수 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260113/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1, b = 2;
+    char c = 'C';
+    string arrow = "->";
+    cout << a << arrow << b << arrow << c;
+    return 0;
+}
