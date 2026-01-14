ede80942fc91247c34f5cf41c4145bb9cb2eec55
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-14 15:35:39 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260114/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num_front, num_back;
+    cin >> num_front;
+    cin.get();
+    cin >> num_back;
+
+    cout << num_front << num_back;
+    
+    return 0;
+}
