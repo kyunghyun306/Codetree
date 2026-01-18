ff61cbb5a28c4a2429e92d27c519635b255fe737
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-18 13:29:15 +0900

    Title: 시력 검사 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260118/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double sight;
+    cin >> sight;
+
+    if(sight >= 1.0) cout << "High";
+    else if(sight >= 0.5) cout << "Middle";
+    else cout << "Low";
+    
+    return 0;
+}
