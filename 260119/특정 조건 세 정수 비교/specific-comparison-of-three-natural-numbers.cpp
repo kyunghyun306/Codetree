d29c0d5566815ba09b043948f5d985b3b9bdfce3
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-19 14:12:47 +0900

    Title: 특정 조건 세 정수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260119/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
+++ 260119/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     int a, b, c;
     cin >> a >> b >> c;
 
-    cout << (a <= b && a <= c) << ' ' << (a == b == c);
-    
+    cout << (a <= b && a <= c) << ' ' << (a == b && a == c);
+
     return 0;
 }
