e0dccd70e07501052bbdece68f302d719f1b03bb
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-21 14:32:19 +0900

    Title: 짝수만 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260121/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int b, a;
+    cin >> b >> a;
+
+    while(b >= a){
+        cout << b << ' ';
+        b -= 2;
+    }
+    
+    return 0;
+}
