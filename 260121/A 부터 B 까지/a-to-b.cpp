c87ac4802c0344b2a55af74b61ac910dc3ad9c32
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-21 15:37:56 +0900

    Title: A 부터 B 까지, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260121/A 부터 B 까지/a-to-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    while(a <= b){
+        cout << a << ' ';
+        if(a % 2) a *= 2;
+        else a += 3;
+    }
+    
+    return 0;
+}
