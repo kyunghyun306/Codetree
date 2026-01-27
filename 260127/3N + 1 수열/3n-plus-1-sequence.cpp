aad591920041c999ece00630e366f2612c1ad247
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-27 11:10:35 +0900

    Title: 3N + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260127/3N + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;;
+    cin >> n;
+
+    while(true){
+        if(n == 1) break;
+
+        if(!(n % 2)) n /= 2;
+        else n = n * 3 + 1;
+        
+        cnt++;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
