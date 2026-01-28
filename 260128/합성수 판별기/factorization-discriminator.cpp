1596abe5ee696ba64ba7540451c0ef5e5dd8a873
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-28 11:10:39 +0900

    Title: 합성수 판별기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260128/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    char result = 'N';
+    cin >> n;
+
+    for(int i = 2; i < n; i++){
+        if(!(n % i)){
+            result = 'C';
+            break;
+        }
+    }
+
+    cout << result;
+    
+    return 0;
+}
