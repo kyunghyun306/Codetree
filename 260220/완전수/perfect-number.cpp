5a0c570e3da626e83a652bc8c0a0659291f3ecb2
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-02-20 14:17:40 +0900

    Title: 완전수, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260220/완전수/perfect-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int st, ed;
+
+int main() {
+    cin >> st >> ed;
+
+    int cnt = 0;
+
+    for(int i = st; i <= ed; i++){
+        int sum = 0;
+        for(int j = 1; j < i; j++){
+            if(!(i % j)) sum += j;
+        }
+        if(sum == i) cnt++;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
