96208486b8b54659e42f88e7fda7e06179441e62
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-02-23 10:35:37 +0900

    Title: 특정 수 도달하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260223/특정 수 도달하기/reaching-specific-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10], sum = 0, cnt = 0;
+
+    for(int i = 0; i < 10; i++) cin >> a[i];
+
+    for(int i = 0; i < 10; i++){
+        if(a[i] >= 250) break;
+        sum += a[i];
+        cnt++;
+    }
+
+    cout << sum << ' ';
+    cout << fixed;
+    cout.precision(1);
+    cout << (double)sum / cnt;
+
+    return 0;
+}
