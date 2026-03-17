09d77cb75c7ed00c99578f650369b1cf880cedfe
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-03-17 14:47:22 +0900

    Title: 나눗셈의 나머지, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260317/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int arr[10] = {0, };
+    int result = 0;
+
+    cin >> a >> b;
+
+    while(a > 0){
+        arr[a % b]++;
+        a /= b;
+    }
+
+    for(int i = 0; i < 10; i++){
+        result = result + (arr[i] * arr[i]);
+    }
+
+    cout << result;
+
+    return 0;
+}
