885b7b8abaefdcf0eb686991c863896b576beb3d
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-03-20 14:04:26 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260320/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    int arr[100];
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 2) cnt ++;
+        if(cnt == 3){
+            cout << i + 1;
+            break;
+        }
+    }
+
+    return 0;
+}
