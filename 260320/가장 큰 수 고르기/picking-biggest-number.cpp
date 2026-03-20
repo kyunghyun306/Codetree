7cd3e503debbfebb43f3469de3c1c9971064a6b1
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-03-20 14:10:37 +0900

    Title: 가장 큰 수 고르기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260320/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+
+    for(int i = 0; i < 10; i++){
+        cin >> arr[i];
+    }
+
+    int max_val = arr[0];
+    for(int i = 1; i < 10; i++){
+        if(arr[i] > max_val) max_val = arr[i];
+    }
+
+    cout << max_val;
+    
+    return 0;
+}
